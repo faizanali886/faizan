7abfa8012345e1f322d48017ffb6a237bab4a99d
Faizan Ali <nfaizanali@CSNA.LAB>
2024-03-01 00:57:34 +0500

    my first push

---
+++ src/Models/ConfigurationFileVersionData.cs
@@ -0,0 +1,18 @@
+﻿namespace AzureNamingTool.Models
+{
+    /// <summary>
+    /// Represents the data for the configuration file version.
+    /// </summary>
+    public class ConfigurationFileVersionData
+    {
+        /// <summary>
+        /// Gets or sets the resource types.
+        /// </summary>
+        public string ResourceTypes { get; set; } = "0.0.0";
+
+        /// <summary>
+        /// Gets or sets the resource locations.
+        /// </summary>
+        public string ResourceLocations { get; set; } = "0.0.0";
+    }
+}
